5a0f1d8a8e70a82d8aa2240fffda6c1baf60fb21
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-11-08 15:32:21 -0800

    Update SQL script to create ingredients and recipe_ingredient tables.

--- initialize.sql
+++ initialize.sql
@@ -10,3 +10,20 @@ CREATE TABLE recipes(
     cookUnit ENUM('min', 'hr') DEFAULT 'min',
     comments TEXT
 );
+
+CREATE TABLE ingredients(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE recipe_ingredient(
+    recipe_id INT NOT NULL,
+    ingredient_id INT NOT NULL,
+
+    amount DECIMAL(5,3),
+    measurement ENUM('teaspoon', 'tablespoon', 'cup', 'ounce', 'pound', 'gram'),
+    size ENUM('small', 'medium', 'large'),
+
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE,
+    FOREIGN KEY (ingredient_id) REFERENCES ingredients(id) ON DELETE CASCADE
+);
