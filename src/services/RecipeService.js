650b480983f93e627e53e8c8fafcb254e98f280d
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-11-13 15:21:58 -0800

    Extracted common variables between methods. Implemented update for recipe info and ingredients.

--- src/services/RecipeService.js
+++ src/services/RecipeService.js
@@ -4,6 +4,21 @@ const Recipe = new Table('recipes');
 const Ingredient = new Table('ingredients');
 const RecipeIngredient = new Table('recipe_ingredient');
 
+const columnsArray = [
+    'recipes.id', 'recipes.title', 'recipes.description', 'recipes.serves', 'recipes.instructions', 'recipes.comments',
+    'JSON_OBJECT("time", recipes.prepTime, "unit", recipes.prepUnit) as prep',
+    'JSON_OBJECT("time", recipes.cookTime, "unit", recipes.cookUnit) as cook',
+    'JSON_OBJECTAGG(i.name, JSON_OBJECT("amount", ri.amount, "measurement", ri.measurement, "size", ri.size)) as ingredients'
+];
+
+const joinsArray = [{
+    table: `${RecipeIngredient.tableName} ri`,
+    on: 'ri.recipe_id = recipes.id'
+}, {
+    table: `${Ingredient.tableName} i`,
+    on: 'i.id = ri.ingredient_id'
+}];
+
 function RecipeService() { }
 
 RecipeService.prototype.addRecipe = async function (recipeInfo) {
@@ -24,9 +39,9 @@ RecipeService.prototype.addRecipe = async function (recipeInfo) {
     for (let ingredient of recipeInfo.ingredients) {
         const ingredientExists = await Ingredient.hasEntry({ name: ingredient.name });
 
-        const ingredientInfo = ingredientExists ?
-            await Ingredient.getEntry({ rows: { name: ingredient.name }}) :
-            await Ingredient.addEntry({ name: ingredient.name });
+        const ingredientId = ingredientExists ?
+            await Ingredient.getEntry({ rows: { name: ingredient.name } }).then(entry => entry.id) :
+            await Ingredient.addEntry({ name: ingredient.name }).then(entry => entry.insertId);
 
         const ingredientAmount = (ingredient.amount && ingredient.amount !== "") ? ingredient.amount : null;
         const ingredientMeasurement = (ingredient.measurement && ingredient.measurement !== "") ? ingredient.measurement : null;
@@ -34,7 +49,7 @@ RecipeService.prototype.addRecipe = async function (recipeInfo) {
 
         await RecipeIngredient.addEntry({
             recipe_id: newRecipe.insertId,
-            ingredient_id: ingredientInfo.id,
+            ingredient_id: ingredientId,
             amount: ingredientAmount,
             measurement: ingredientMeasurement,
             size: ingredientSize,
@@ -45,37 +60,92 @@ RecipeService.prototype.addRecipe = async function (recipeInfo) {
 }
 
 RecipeService.prototype.getRecipes = async function () {
-    return Recipe.getEntries();
+    const recipes = await Recipe.getEntries({
+        columns: columnsArray,
+        joins: joinsArray,
+        groupBy: 'recipes.id'
+    });
+
+    if (!recipes.length) return;
+
+    return recipes.map(recipe => ({
+        ...recipe,
+        prep: JSON.parse(recipe.prep),
+        cook: JSON.parse(recipe.cook),
+        ingredients: JSON.parse(recipe.ingredients),
+        instructions: recipe.instructions.split("|"),
+        comments: recipe.comments && recipe.comments.split("|")
+    }))
 }
 
 RecipeService.prototype.getRecipe = async function (recipeId) {
-    const recipe = await Recipe.getEntry({ 
+    const recipe = await Recipe.getEntry({
         rows: { 'recipes.id': recipeId },
-        columns: [
-            'recipes.id', 'recipes.title', 'recipes.description', 'recipes.serves', 'recipes.instructions', 'recipes.comments',
-            'JSON_OBJECTAGG(i.name, JSON_OBJECT("amount", ri.amount, "measurement", ri.measurement, "size", ri.size)) as ingredients'
-        ],
-        joins: [{
-            table: 'recipe_ingredient ri',
-            on: 'ri.recipe_id = recipes.id'
-        }, {
-            table: 'ingredients i',
-            on: 'i.id = ri.ingredient_id'
-        }]
+        columns: columnsArray,
+        joins: joinsArray
     });
 
-    if(!recipe.id) return;
-    
+    if (!recipe.id) return;
+
     return {
         ...recipe,
-        comments: recipe.comments && recipe.comments.split("|"),
+        prep: JSON.parse(recipe.prep),
+        cook: JSON.parse(recipe.cook),
+        ingredients: JSON.parse(recipe.ingredients),
         instructions: recipe.instructions.split("|"),
-        ingredients: JSON.parse(recipe.ingredients)
+        comments: recipe.comments && recipe.comments.split("|")
     }
 }
 
 RecipeService.prototype.updateRecipe = async function (recipeId, updates) {
-    await Recipe.updateEntries({ id: recipeId }, updates);
+    const { title, description, instructions, comments, serves, prep, cook, ingredients } = updates;
+
+    await Recipe.updateEntries({ id: recipeId }, {
+        title, description, serves,
+        instructions: instructions?.join("|"),
+        comments: comments?.join("|"),
+        prepTime: prep?.time, prepUnit: prep?.unit,
+        cookTime: cook?.time, cookUnit: cook?.unit,
+    });
+
+    for (let ingredientName in ingredients) {
+        const ingredient = await Ingredient.getEntry({ rows: { name: ingredientName } });
+        const isRemoving = ingredients[ingredientName] === null;
+        
+        if(!ingredient && isRemoving) continue;
+        
+        const ingredientId = ingredient ? ingredient.id : await Ingredient.addEntry({ name: ingredientName }).then(entry => entry.insertId);
+        
+        const existsInRecipe = await RecipeIngredient.getEntry({ rows: { recipe_id: recipeId, ingredient_id: ingredient.id } });
+
+        if(existsInRecipe && isRemoving) {
+            await RecipeIngredient.removeEntries({ recipe_id: recipeId, ingredient_id: ingredientId });
+            continue;
+        }
+
+        if(!existsInRecipe && isRemoving) continue;
+
+        const ingredientAmount = (ingredients[ingredientName]?.amount !== "") ? ingredients[ingredientName]?.amount : null;
+        const ingredientMeasurement = (ingredients[ingredientName]?.measurement !== "") ? ingredients[ingredientName]?.measurement : null;
+        const ingredientSize = (ingredients[ingredientName]?.size !== "") ? ingredients[ingredientName]?.size : null;
+        
+        if(existsInRecipe) {
+            await RecipeIngredient.updateEntries(
+                { recipe_id: recipeId, ingredient_id: ingredientId },
+                { amount: ingredientAmount, measurement: ingredientMeasurement, size: ingredientSize }
+            );
+            continue;
+        }
+
+        await RecipeIngredient.addEntry({
+            recipe_id: recipeId,
+            ingredient_id: ingredientId,
+            amount: ingredientAmount,
+            measurement: ingredientMeasurement,
+            size: ingredientSize,
+        })
+    }
+
     return this.getRecipe(recipeId);
 }
 
