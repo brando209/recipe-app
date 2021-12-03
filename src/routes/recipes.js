a35f58fe1bd9b3bc1c1e61a5f533fe71344c9c53
Branden McCarthy <bmccarthy2@ucmerced.edu>
2021-12-03 11:11:12 -0800

    Added user authorzation to certain recipe routes. Passed user id to necessary `RecipeService` methods.

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-
 const RecipeService = require('../services/RecipeService');
 const service = new RecipeService();
 
 const { validateRecipe } = require('../middlewares/validation');
-const { authorizeJWT } = require('../middlewares/authorization');
+const { authorizeJWT, authorizeUser } = require('../middlewares/authorization');
 
 router.use(authorizeJWT);
 
 router.get('/', async (req, res) => {
-    const recipes = await service.getRecipes();
+    const recipes = await service.getRecipes(req.user.id);
     return res.send(recipes);
 });
 
-router.get('/:recipeId', async (req, res) => {
+router.get('/:recipeId', authorizeUser, async (req, res) => {
     const recipeId = req.params.recipeId;
-    const recipe = await service.getRecipe(recipeId);
+    const recipe = await service.getRecipe(recipeId, req.user.id);
     if(recipe) return res.send(recipe);
     return res.status(404).send("Recipe not found!");
 });
@@ -26,7 +25,7 @@ router.post('/', validateRecipe, async (req, res) => {
     const recipeInfo = req.body;
 
     try {
-        const newRecipe = await service.addRecipe(recipeInfo);
+        const newRecipe = await service.addRecipe(recipeInfo, req.user.id);
         if(!newRecipe) return res.status(400).send("Recipe not created!");
         return res.json(newRecipe);
     } catch(err) {
@@ -35,7 +34,7 @@ router.post('/', validateRecipe, async (req, res) => {
     }
 });
 
-router.delete('/:recipeId', async (req, res) => {
+router.delete('/:recipeId', authorizeUser, async (req, res) => {
     const recipeId = req.params.recipeId;
 
     try {
@@ -47,11 +46,11 @@ router.delete('/:recipeId', async (req, res) => {
     }
 });
 
-router.patch('/:recipeId', async (req, res) => {
+router.patch('/:recipeId', authorizeUser, async (req, res) => {
     const recipeId = req.params.recipeId;
     
     try {
-        const updatedRecipe = await service.updateRecipe(recipeId, req.body);
+        const updatedRecipe = await service.updateRecipe(recipeId, req.body, req.user.id);
         res.json(updatedRecipe);
     } catch(err) {
         console.log(err);
