7981855024c3a223945e16349168cdb8bd44fbd4
Branden McCarthy <bmccarthy2@ucmerced.edu>
2022-03-18 13:37:10 -0700

    Set database timezone to 'UTC'

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,7 +4,8 @@ const connectionOptions = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_SECRET,
-    database: process.env.NODE_ENV === 'test' ? process.env.DB_TEST : process.env.DB_NAME
+    database: process.env.NODE_ENV === 'test' ? process.env.DB_TEST : process.env.DB_NAME,
+    timezone: 'Z'
 };
 
 const connection = process.env.JAWSDB_URL ? mysql.createConnection(process.env.JAWSDB_URL) : mysql.createConnection(connectionOptions);
